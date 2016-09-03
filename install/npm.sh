e8f2476b1ec5a7087eca5cbc600ed16452739349
Nick Plekhanov <nickatgit@yandex.com>
2016-09-04 01:16:06 +0200

    npm: Reflect recent changes to used npm packages

--- install/npm.sh
+++ install/npm.sh
@@ -17,27 +17,38 @@ if test ! $(which nvm)
 then
   echo "Installing a stable version of Node..."
 
-  # Install the stable version of node.
-  nvm install 5
+  # Install the latest stable version of node
+  nvm install stable
 
   # Switch to the installed version
-  nvm use 5
+  nvm use node
 
-  # Use the stable version of node by default.
-  nvm alias default 5
+  # Use the stable version of node by default
+  nvm alias default node
 fi
 
-# All `npm install <pkg>`` commands will pin to the version that was available at the time you run the command
+# All `npm install <pkg>` commands will pin to the version that was available at the time you run the command
 npm config set save-exact = true
 
 # Globally install with npm
+# To list globally installed npm packages and version: npm list -g --depth=0
+#
+# Some descriptions:
+#
+# diff-so-fancy — sexy git diffs
+# git-recent — Type `git recent` to see your recent local git branches
+# git-open — Type `git open` to open the GitHub page or website for a repository
 packages=(
     diff-so-fancy
+    git-recent
+    git-open
     gulp
-    grunt-cli
-    bower
     http-server
     servedir
+    flow-bin
+    flow-typed
+    npm-check-updates
+    webpack
     nodemon
     svgo
     yo
