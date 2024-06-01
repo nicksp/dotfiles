6c4193d0d879e35e7283e7cfb93016df372e4267
Nick Plekhanov <nicksp@users.noreply.github.com>
2024-06-01 12:44:43 +0200

    WezTerm: Update config file to use spaces for consistency with other files

--- tilde/.wezterm.lua
+++ tilde/.wezterm.lua
@@ -7,16 +7,16 @@ local session_manager = require 'wezterm-session-manager/session-manager'
 -- Fonts
 local font = 'MonoLisa'
 config.font = wezterm.font_with_fallback({
-	{ family = font, weight = 'Regular', italic = false },
+  { family = font, weight = 'Regular', italic = false },
   { family = 'Symbols Nerd Font Mono', scale = 1 },
 })
 config.font_rules = {
-	{
-		intensity = 'Bold',
-		font = wezterm.font_with_fallback({
-			{ family = font, weight = 'Regular', italic = false, weight = 'Bold' },
-			{ family = 'Symbols Nerd Font Mono', scale = 1 },
-		}),
+  {
+    intensity = 'Bold',
+    font = wezterm.font_with_fallback({
+      { family = font, weight = 'Regular', italic = false, weight = 'Bold' },
+      { family = 'Symbols Nerd Font Mono', scale = 1 },
+    }),
   }
 }
 
@@ -27,11 +27,11 @@ config.harfbuzz_features = { 'calt=1', 'clig=0', 'liga=0', 'zero', 'ss01' }
 config.color_scheme = 'Squirrelsong Dark'
 
 config.window_frame = {
-	font = wezterm.font { family = font, weight = 'Bold' },
-	font_size = 15,
-	-- Fancy tab bar
-	active_titlebar_bg = '#574131',
-	inactive_titlebar_bg = '#352a21',
+  font = wezterm.font { family = font, weight = 'Bold' },
+  font_size = 15,
+  -- Fancy tab bar
+  active_titlebar_bg = '#574131',
+  inactive_titlebar_bg = '#352a21',
 }
 
 -- Command Palette
@@ -43,65 +43,65 @@ config.command_palette_fg_color = "#c4a389"
 -- Hot keys
 local act = wezterm.action
 config.keys = {
-	-- Make Page up/down work
-	{ key = 'PageUp', action = act.ScrollByPage(-1) },
-	{ key = 'PageDown', action = act.ScrollByPage(1) },
+  -- Make Page up/down work
+  { key = 'PageUp', action = act.ScrollByPage(-1) },
+  { key = 'PageDown', action = act.ScrollByPage(1) },
 
-	-- Pane splitting
-	{
-		key = 'd',
-		mods = 'CMD',
-		action = act.SplitHorizontal({ domain = 'CurrentPaneDomain' }),
-	},
-	{
-		key = 'w',
-		mods = 'CMD',
-		action = act.CloseCurrentPane({ confirm = false }),
-	},
+  -- Pane splitting
+  {
+    key = 'd',
+    mods = 'CMD',
+    action = act.SplitHorizontal({ domain = 'CurrentPaneDomain' }),
+  },
+  {
+    key = 'w',
+    mods = 'CMD',
+    action = act.CloseCurrentPane({ confirm = false }),
+  },
 
-	-- Switch between tabs
-	{
-		key = 'LeftArrow',
-		mods = 'CMD|ALT',
-		action = act.ActivateTabRelative(-1),
-	},
-	{
-		key = 'RightArrow',
-		mods = 'CMD|ALT',
-		action = act.ActivateTabRelative(1),
-	},
+  -- Switch between tabs
+  {
+    key = 'LeftArrow',
+    mods = 'CMD|ALT',
+    action = act.ActivateTabRelative(-1),
+  },
+  {
+    key = 'RightArrow',
+    mods = 'CMD|ALT',
+    action = act.ActivateTabRelative(1),
+  },
 
-	-- Switch between panes
-	{
-		key = 'LeftArrow',
-		mods = 'CMD|SHIFT',
-		action = act.ActivatePaneDirection('Prev'),
-	},
-	{
-		key = 'RightArrow',
-		mods = 'CMD|SHIFT',
-		action = act.ActivatePaneDirection('Next'),
-	},
+  -- Switch between panes
+  {
+    key = 'LeftArrow',
+    mods = 'CMD|SHIFT',
+    action = act.ActivatePaneDirection('Prev'),
+  },
+  {
+    key = 'RightArrow',
+    mods = 'CMD|SHIFT',
+    action = act.ActivatePaneDirection('Next'),
+  },
 
-	-- Jump word to the left
-	{
-		key = 'LeftArrow',
-		mods = 'OPT',
-		action = act.SendKey({ key = 'b', mods = 'ALT' }),
-	},
-	-- Jump word to the right
-	{
-		key = 'RightArrow',
-		mods = 'OPT',
-		action = act.SendKey({ key = 'f', mods = 'ALT' }),
-	},
+  -- Jump word to the left
+  {
+    key = 'LeftArrow',
+    mods = 'OPT',
+    action = act.SendKey({ key = 'b', mods = 'ALT' }),
+  },
+  -- Jump word to the right
+  {
+    key = 'RightArrow',
+    mods = 'OPT',
+    action = act.SendKey({ key = 'f', mods = 'ALT' }),
+  },
 
-	-- Display tab navigator
-	{
-		key = 't',
-		mods = 'CMD|SHIFT',
-		action = act.ShowTabNavigator,
-	},
+  -- Display tab navigator
+  {
+    key = 't',
+    mods = 'CMD|SHIFT',
+    action = act.ShowTabNavigator,
+  },
 
   -- Rename tab title
   {
@@ -155,45 +155,45 @@ config.keys = {
     action = act.TogglePaneZoomState
   },
 
-	-- TODO: Select character to the left
-	{
-		key = 'LeftArrow',
-		mods = 'SHIFT',
-		action = act.DisableDefaultAssignment,
-	},
-	-- TODO: Select character to the right
-	{
-		key = 'RightArrow',
-		mods = 'SHIFT',
-		action = act.DisableDefaultAssignment,
-	},
+  -- TODO: Select character to the left
+  {
+    key = 'LeftArrow',
+    mods = 'SHIFT',
+    action = act.DisableDefaultAssignment,
+  },
+  -- TODO: Select character to the right
+  {
+    key = 'RightArrow',
+    mods = 'SHIFT',
+    action = act.DisableDefaultAssignment,
+  },
 
-	-- Go to beginning of line
-	{
-		key = 'LeftArrow',
-		mods = 'CMD',
-		action = act.SendKey({ key = 'a', mods = 'CTRL' }),
-	},
-	-- Go to end of line
-	{
-		key = 'RightArrow',
-		mods = 'CMD',
-		action = act.SendKey({ key = 'e', mods = 'CTRL' }),
-	},
+  -- Go to beginning of line
+  {
+    key = 'LeftArrow',
+    mods = 'CMD',
+    action = act.SendKey({ key = 'a', mods = 'CTRL' }),
+  },
+  -- Go to end of line
+  {
+    key = 'RightArrow',
+    mods = 'CMD',
+    action = act.SendKey({ key = 'e', mods = 'CTRL' }),
+  },
 
-	-- Case-insensitive search
-	{
-		key = 'f',
-		mods = 'CMD',
-		action = act.Search({ CaseInSensitiveString = '' }),
-	},
+  -- Case-insensitive search
+  {
+    key = 'f',
+    mods = 'CMD',
+    action = act.Search({ CaseInSensitiveString = '' }),
+  },
 
-	-- Disable some default hotkeys
-	{
-		key = 'Enter',
-		mods = 'OPT',
-		action = act.DisableDefaultAssignment,
-	},
+  -- Disable some default hotkeys
+  {
+    key = 'Enter',
+    mods = 'OPT',
+    action = act.DisableDefaultAssignment,
+  },
 
   -- Show Command Palette
   {
@@ -202,29 +202,29 @@ config.keys = {
     action = act.ActivateCommandPalette,
   },
 
-	-- Session Manager
-	{ key = 's', mods = 'CMD|OPT', action = act { EmitEvent = 'save_session' } },
-	{ key = 'r', mods = 'CMD|OPT', action = act { EmitEvent = 'restore_session' } },
+  -- Session Manager
+  { key = 's', mods = 'CMD|OPT', action = act { EmitEvent = 'save_session' } },
+  { key = 'r', mods = 'CMD|OPT', action = act { EmitEvent = 'restore_session' } },
 
-	-- TODO: Show/Hide terminal https://github.com/wez/wezterm/issues/1751
+  -- TODO: Show/Hide terminal https://github.com/wez/wezterm/issues/1751
 }
 
 -- Mouse
 config.mouse_bindings = {
-	-- Change the default click behavior so that it only selects
-	-- text and doesn't open hyperlinks
-	{
-		event = { Up = { streak = 1, button = 'Left' } },
-		mods = 'NONE',
-		action = act.CompleteSelection('ClipboardAndPrimarySelection'),
-	},
+  -- Change the default click behavior so that it only selects
+  -- text and doesn't open hyperlinks
+  {
+    event = { Up = { streak = 1, button = 'Left' } },
+    mods = 'NONE',
+    action = act.CompleteSelection('ClipboardAndPrimarySelection'),
+  },
 
-	-- Open links on Option+Click
-	{
-		event = { Up = { streak = 1, button = 'Left' } },
-		mods = 'OPT',
-		action = act.OpenLinkAtMouseCursor,
-	},
+  -- Open links on Option+Click
+  {
+    event = { Up = { streak = 1, button = 'Left' } },
+    mods = 'OPT',
+    action = act.OpenLinkAtMouseCursor,
+  },
 
    -- Paste on Right click
   {
@@ -237,11 +237,11 @@ config.mouse_bindings = {
 -- Visual bell
 config.audible_bell = 'Disabled'
 config.visual_bell = {
-	target = "CursorColor",
-	fade_in_function = "EaseIn",
-	fade_in_duration_ms = 150,
-	fade_out_function = "EaseOut",
-	fade_out_duration_ms = 300,
+  target = "CursorColor",
+  fade_in_function = "EaseIn",
+  fade_in_duration_ms = 150,
+  fade_out_function = "EaseOut",
+  fade_out_duration_ms = 300,
 }
 
 -- Misc
@@ -267,25 +267,25 @@ config.window_background_opacity = 0.95
 config.window_padding = { left = 12, right = 8, top = 12, bottom = 8}
 
 local function get_current_working_dir(tab)
-	local current_dir = tab.active_pane and tab.active_pane.current_working_dir or { file_path = '' }
-	local HOME_DIR = string.format('file://%s', os.getenv('HOME'))
+  local current_dir = tab.active_pane and tab.active_pane.current_working_dir or { file_path = '' }
+  local HOME_DIR = string.format('file://%s', os.getenv('HOME'))
 
-	return current_dir == HOME_DIR and '.'
-	or string.gsub(current_dir.file_path, '(.*[/\\])(.*)', '%2')
+  return current_dir == HOME_DIR and '.'
+  or string.gsub(current_dir.file_path, '(.*[/\\])(.*)', '%2')
 end
 
 -- Set tab title to the one that was set via `tab:set_title()`
 -- or fall back to the current working directory as a title
 wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
-	local index = tonumber(tab.tab_index) + 1
-	local custom_title = tab.tab_title
-	local title = get_current_working_dir(tab)
+  local index = tonumber(tab.tab_index) + 1
+  local custom_title = tab.tab_title
+  local title = get_current_working_dir(tab)
 
-	if custom_title and #custom_title > 0 then
-		title = custom_title
-	end
+  if custom_title and #custom_title > 0 then
+    title = custom_title
+  end
 
-	return string.format('  %s•%s  ', index, title)
+  return string.format('  %s•%s  ', index, title)
 end)
 
 -- Session manager
@@ -294,15 +294,15 @@ wezterm.on('restore_session', function(window) session_manager.restore_state(win
 
 -- Set window title to the current working directory
 wezterm.on('format-window-title', function(tab, pane, tabs, panes, config)
-	return get_current_working_dir(tab)
+  return get_current_working_dir(tab)
 end)
 
 -- Set the correct window size at the startup
 wezterm.on('gui-startup', function(cmd)
-	local _, _, window = wezterm.mux.spawn_window(cmd or {})
+  local _, _, window = wezterm.mux.spawn_window(cmd or {})
 
-	-- Open full screen
-	window:gui_window():maximize()
+  -- Open full screen
+  window:gui_window():maximize()
 end)
 
 --- 8< -- 8< ---
