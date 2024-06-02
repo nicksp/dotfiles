f52a9bdcda22cf5b2c3dd5f2f3afdff8cb0839af
Nick Plekhanov <nicksp@users.noreply.github.com>
2024-06-02 13:39:26 +0200

    clonecd: Add support for pnpm

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -136,13 +136,17 @@ mkcd() {
 # git clone and cd to a repo directory
 clonecd() {
   git clone "$@"
+
   if [ "$2" ]; then
     cd "$2"
   else
     cd $(basename "$1" .git)
   fi
+
   if [[ -r "./yarn.lock" ]]; then
     yarn
+  elif [[ -r "./pnpm-lock.yaml" ]]; then
+    pnpm install
   elif [[ -r "./package-lock.json" ]]; then
     npm install
   fi
