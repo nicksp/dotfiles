be666ce438a1069d79a36f448d35de81d8c7b5b3
Nick Plekhanov <nicksp@users.noreply.github.com>
2025-12-20 00:32:40 +0100

    Brew : Delete apps I no longer use
    - Things (→ Reminders)
    - Colorslurp (→ Contrast a+ Raycast plugin)

--- setup/brew.sh
+++ setup/brew.sh
@@ -45,9 +45,7 @@ echo "${bold}Audirvāna Origin${reset} − https://audirvana.com/audirvana-origi
 echo ""
 title "🍏 Install additional apps from App Store:"
 echo "https://apps.apple.com/us/app/adguard-for-safari/id1440147259?mt=12"
-echo "https://apps.apple.com/us/app/colorslurp/id1287239339"
 echo "https://apps.apple.com/us/app/klack/id6446206067?mt=12"
-echo "https://apps.apple.com/us/app/things-3/id904280696?mt=12"
 echo "https://apps.apple.com/us/app/pages/id409201541?mt=12"
 
 # Remove outdated versions of formulae and casks from the cellar
